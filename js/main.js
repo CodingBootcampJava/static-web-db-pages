909261e3ce6619858bcfd79385d12ed5e480a697
Gratsiella Kalfakakou <37407309+Gratsiella@users.noreply.github.com>
2020-04-23 02:25:36 +0300

    initial

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function() {
+  $(".list-group-item").on("click", function() {
+    $(".far", this)
+      .toggleClass("fa-arrow-alt-circle-right")
+      .toggleClass("fa-arrow-alt-circle-down");
+  });
+});
